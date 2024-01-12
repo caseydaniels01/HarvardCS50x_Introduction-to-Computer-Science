d1cb658286e11af244b800819eb28c7bab43458d
caseydaniels01 <caseydaniels01@gmail.com>
2024-01-11 23:40:07 -0600

    Start_Scrabble

---
+++ week2/scrabble.c
@@ -0,0 +1,59 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int calc_points[] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
+
+int calc(string word_check)
+{
+    int points = 0;
+    for (int i = 0, len = strlen(word_check); i < len; i++)
+    {
+        points += calc_points[word_check[i] - 'A'];
+    }
+    return points;
+}
+
+int main(void)
+{
+
+char first_word[100];
+printf("Player 1: ");
+scanf("%s", first_word);
+char first_word_upper[100];
+int p1i = 0;
+    while(first_word[p1i])
+    {
+      first_word[p1i] = toupper(first_word[p1i]);
+      p1i++;
+    }
+
+char second_word[100];
+printf("Player 2: ");
+scanf("%s", second_word);
+
+    int p2i = 0;
+    while(second_word[p2i])
+    {
+        second_word[p2i] = toupper(second_word[p2i]);
+        p2i++;
+    }
+
+int p1s = calc(first_word);
+int p2s = calc(second_word);
+
+if ( p1s > p2s)
+{
+    printf("Player 1 wins!\n");
+}
+else if ( p1s < p2s)
+{
+    printf("Player 2 wins!\n");
+}
+else
+{
+    printf("Tie!\n");
+}
+
+}
