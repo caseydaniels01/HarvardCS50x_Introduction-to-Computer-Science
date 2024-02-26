97b9ecff5e8b565df616d00b8efc8b47c15f7095
caseydaniels01 <caseydaniels01@gmail.com>
2024-02-25 21:37:36 -0600

    Update movies7.sql

--- week7/movies7.sql
+++ week7/movies7.sql
@@ -1,1 +1,1 @@
-SELECT title, rating FROM movies JOIN ratings ON ratings.movie_id = movies.id WHERE movies.year = '2012' ORDER BY rating DESC, title;
+SELECT title, rating FROM movies JOIN ratings ON ratings.movie_id = movies.id WHERE movies.year = '2010' ORDER BY rating DESC, title;
